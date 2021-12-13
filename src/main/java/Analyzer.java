c49e7090d4468c5219b29ae2aa7294bb3edc2757
DalVlad <71518602+DalVlad@users.noreply.github.com>
2021-12-13 23:45:15 +0300

    Add files via upload

---
+++ src/main/java/Analyzer.java
@@ -0,0 +1,53 @@
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Analyzer {
+    private HashMap<String, Long> sizes;
+
+    public Map<String, Long> calculateDirectorySize(Path path) {
+        try {
+            sizes = new HashMap<>();
+            Files.walkFileTree(
+                    path,
+                    new SimpleFileVisitor<>() {
+                        @Override
+                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                            try {
+                                long size = Files.size(file);
+                                updateDirSize(file, size);
+                            }catch (IOException e) {
+                                System.out.println(new RuntimeException(e) + "\n");
+                            }
+                            return FileVisitResult.CONTINUE;
+                        }
+
+                        @Override
+                        public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                            return FileVisitResult.SKIP_SUBTREE;
+                        }
+                    }
+            );
+            return sizes;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void updateDirSize(Path path, Long size) {
+        String key = path.toString();
+        sizes.put(key, size + sizes.getOrDefault(key, 0L));
+
+        Path parent = path.getParent();
+
+        if (parent != null) {
+            updateDirSize(parent, size);
+        }
+    }
+}
